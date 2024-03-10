81fdb13016a7c9f8ef8b4d360e1f0f8463e03818
TeXmeijin <i8858garden@gmail.com>
2024-03-10 18:24:42 +0900

    feat: 🎸 header

--- src/app/(lens-system)/layout.tsx
+++ src/app/(lens-system)/layout.tsx
@@ -7,10 +7,29 @@ export default function RootLayout({
 }>) {
   return (
     <div>
-      <header className={"p-4 bg-gray-100"}>
+      <header className={"p-4 bg-gray-100 flex justify-between"}>
         <Link href={"/"}>
-          <h1 className={"text-xl font-thin"}>レンズ比較.com</h1>
+          <h1 className={"text-xl font-thin"}>レンズシステム計画ツール</h1>
         </Link>
+        <div className="flex items-center">
+          <Link href={"/lmount-lens"}>
+            <span className={"px-2"}>Lマウント</span>
+          </Link>
+          <Link
+            className={"px-2 flex gap-x-0.5 text-gray-400 items-center"}
+            href={"#"}
+          >
+            <span>Zマウント</span>
+            <span className={"text-xs font-bold"}>近日公開</span>
+          </Link>
+          <Link
+            className={"px-2 flex gap-x-0.5 text-gray-400 items-center"}
+            href={"#"}
+          >
+            <span>RFマウント</span>
+            <span className={"text-xs font-bold"}>近日公開</span>
+          </Link>
+        </div>
       </header>
       {children}
     </div>
